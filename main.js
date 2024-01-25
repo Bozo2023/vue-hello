c4284f309fd4a8bb811bf4306911324903e85302
Bozo2023 <ulrob1.0@email.com>
2024-01-25 18:36:52 +0100

    kemet

---
+++ main.js
@@ -0,0 +1,24 @@
+const{ createApp} =Vue
+createApp({
+    data() {
+        return{
+            message: "Hello. This is introduction lesson of Vue",
+            linkImg: "./assest/img/images.jpg"
+        }
+    }
+}).mount("#app")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
